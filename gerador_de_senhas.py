8e60e03fadec5522ea83b1bf06cc212a14785bba
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-03-03 09:17:40 -0300

    allow-empty

---
+++ gerador_de_senhas.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+tamanho = int(input("Digite o tamanho de senha que deseja: "))
+
+chars = string.ascii_letters + string.digits + '!@#$%&*()-+=,.;:/?[{<>}]'
+
+rnd = random.SystemRandom()
+
+print("Senha gerada: ")
+print("".join(rnd.choice(chars) for i in range(tamanho)))
