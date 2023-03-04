65692a0c14b23d1cf3fc60edbbf66d6275c5d02e
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-03-04 09:51:13 -0300

    allow-empty

--- gerador_de_senhas.py
+++ gerador_de_senhas.py
@@ -5,7 +5,7 @@ tamanho = int(input("Digite o tamanho de senha que deseja: "))
 
 chars = string.ascii_letters + string.digits + '!@#$%&*()-+=,.;:/?[{<>}]'
 
-rnd = random.SystemRandom()
+rnd = random.SystemRandom() 
 
 print("Senha gerada: ")
 print("".join(rnd.choice(chars) for i in range(tamanho)))
