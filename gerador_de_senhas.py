3516989d393f5521bb0ef6aac9d581bee26614a7
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-04-12 11:00:15 -0300

    allow-empty

--- gerador_de_senhas.py
+++ gerador_de_senhas.py
@@ -1,12 +1,19 @@
 import random
 import string
 
+# Pede o número desejado, de 1 a n 
 tamanho = int(input("Digite o tamanho de senha que deseja: "))
 
-chars = string.ascii_letters + string.digits + '!@#$%&*()-+=,.;:/?[{<>}]'
+# Coloca quais caracteres podem entrar na senha gerada
+chars = string.ascii_letters + string.digits + '!@#$%&*()-+=,.;:/?[{<>}]qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890'
 
+
+# Algoritmo de aleatoriedade
 rnd = random.SystemRandom() 
 
+# Mostra a senha gerada
 print("Senha gerada: ")
+
+# Gera a senha a ser mostrada
 print("".join(rnd.choice(chars) for i in range(tamanho)))
  
