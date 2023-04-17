a81cac8af0ceb8322c6277f63315091d6fd6a29f
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-04-17 07:17:30 -0300

    allow-empty

--- Web/index.php
+++ Web/index.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Gerador de Senhas</title>
 </head>
 <body>
+	<div>
 	<h1>Gerador de Senhas</h1>
 	<form action="" method="post">
 		Tamanho da senha: <input type="number" name="tamanho" min="1" max="100"><br><br>
@@ -21,6 +24,9 @@
 <br><br><br>
 <footer>
 	<a target="_blank" href="https://leonardostella.lesttech.com.br/">Desenvolvido por Leonardo Stella</a>
+</footer>
+</div>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
 
